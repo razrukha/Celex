b07d11363596a9ca67abb8ed32b65c2db705f42c
matas <85616020+razrukha@users.noreply.github.com>
2024-03-08 12:50:34 +0000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1474,7 +1474,7 @@ do -- Library
                     Binds.Objects["Text_Outline"] = Library.Objects:Text(Vector2.new(0, 0), Binds.Objects["Frame"], UDim2.new(0, 1, 0, 5), UDim2.new(1, 0, 0, 15), "", "Outline", "Center", 1000)
                     Binds.Objects["Text"] = Library.Objects:Text(nil, Binds.Objects["Text_Outline"], UDim2.new(0, -1, 0, -1), UDim2.new(1, 0, 1, 0), "Binds", "Light Text", "Center", 1000)
                     --
-                    Binds.Objects["Holder_Outline"], Binds.Objects["Holder_Frame"] = Library.Objects:Outline(nil, Binds.Objects["Frame"], UDim2.new(0, 2, 0, 22), UDim2.new(1, -4, 1, -24), "Lighter Background", nil, 1000)
+                    Binds.Objects["Holder_Outline"], Binds.Objects["Holder_Frame"] = Library.Objects:Outline(nil, Binds.Objects["Frame"], UDim2.new(0, 2, 0, 22), UDim2.new(1, -4, 1, -24), "Lighter Background", nil, nil, 1000)
                     --
                     Binds.Objects["Holder"] = Library.Objects:Holder(nil, Binds.Objects["Holder_Frame"], UDim2.new(0, 0, 0, 2), UDim2.new(1, 0, 0, -4))
                 end
